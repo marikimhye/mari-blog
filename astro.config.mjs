6fd138e8f15c32fc6d67a9b27ccc25e32d57f6b7
marikimhye <marikimhye@gamil.com>
2026-03-08 15:00:54 +0900

    Fix: site URL to marie-blog.pages.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mari-blog.pages.dev',
+  site: 'https://marie-blog.pages.dev',
   integrations: [mdx(), tailwind()]
 });
